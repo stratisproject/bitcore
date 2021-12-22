13552ee896f5e8ff9de62ade45f3a302f298ad0f
phr4x <phr4x@users.noreply.github.com>
2021-12-22 00:00:00 +0000

    Update build script to support release build, signing and zipaligning

--- Docker/ionic/build-android.sh
+++ Docker/ionic/build-android.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# ARGS: Arg 1 is the type of build. Use build:android for debug and build:android-release for release
+# Arg 2 is the keystore password
+
+# Attempt to prevent Node out of memory errors
+export NODE_OPTIONS="--max-old-space-size=8192"
+
+echo Running build: ${1:-"build:android"}
 git clone https://github.com/stratisproject/bitcore-stratis.git --recurse-submodules --single-branch --branch strax-cirrus /app/bitcore
 cd /app/bitcore
 npm i --unsafe-perm
@@ -10,5 +17,9 @@ mv /app/google-services.json /app/bitcore/packages/mobile-wallet/platforms/andro
 cd /app/bitcore
 npm i --unsafe-perm
 cd /app/bitcore/packages/mobile-wallet
-npm run build:android
+npm run ${1:-"build:android"}
 cp -r /app/bitcore/packages/mobile-wallet/platforms/android/app/build/outputs/apk /output
+
+# IF release, sign and zipalign
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /keys/stratiswallet.keystore -storepass ${2} -signedjar /output/apk/release/app-release-signed.apk /output/apk/release/app-release-unsigned.apk  stratiswallet
+$ANDROID_HOME/build-tools/27.0.0/zipalign -v 4 /output/apk/release/app-release-signed.apk /output/android-release-signed-aligned.apk
