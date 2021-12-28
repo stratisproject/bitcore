84975de90d58bf70e8f787756e2ff6037f654737
phr4x <phr4x@users.noreply.github.com>
2021-12-28 00:00:00 +0000

    Update build script

--- Docker/ionic/build-android-release.sh
+++ Docker/ionic/build-android-release.sh
@@ -15,7 +15,6 @@ npm i --unsafe-perm
 cd /app/bitcore/packages/mobile-wallet
 npm run apply:stratis
 ionic cordova platform add android
-mv /app/google-services.json /app/bitcore/packages/mobile-wallet/platforms/android/app/google-services.json
 cd /app/bitcore
 npm i --unsafe-perm
 cd /app/bitcore/packages/mobile-wallet
