c85726cb47764e729272b4e60577980177b28ded
phr4x <phr4x@users.noreply.github.com>
2021-12-28 00:00:00 +0000

    Update ionic build

--- Docker/ionic/build-android-release.sh
+++ Docker/ionic/build-android-release.sh
@@ -6,7 +6,8 @@
 # Attempt to prevent Node out of memory errors
 export NODE_OPTIONS="--max-old-space-size=8192"
 
-echo Running build: ${1:-"build:android"}
+echo "Running build: build:stratis:android-release (release)"
+rm -rf /app/bitcore
 git clone https://github.com/stratisproject/bitcore-stratis.git --recurse-submodules --single-branch --branch strax-cirrus /app/bitcore
 cd /app/bitcore
 npm i --unsafe-perm
@@ -17,9 +18,8 @@ mv /app/google-services.json /app/bitcore/packages/mobile-wallet/platforms/andro
 cd /app/bitcore
 npm i --unsafe-perm
 cd /app/bitcore/packages/mobile-wallet
-npm run ${1:-"build:android"}
+npm run build:stratis:android-release
 cp -r /app/bitcore/packages/mobile-wallet/platforms/android/app/build/outputs/apk /output
 
-# IF release, sign and zipalign
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /keys/stratiswallet.keystore -storepass ${2} -signedjar /output/apk/release/app-release-signed.apk /output/apk/release/app-release-unsigned.apk  stratiswallet
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /keys/stratiswallet.keystore -storepass ${1} -signedjar /output/apk/release/app-release-signed.apk /output/apk/release/app-release-unsigned.apk  stratiswallet
 $ANDROID_HOME/build-tools/27.0.0/zipalign -v 4 /output/apk/release/app-release-signed.apk /output/apk/release/android-release-signed-aligned.apk
