0c7dda65002108074f4d6710d81680ff4c6a03db
phr4x <phr4x@users.noreply.github.com>
2022-01-13 00:00:00 +0000

    Update build script

--- Docker/ionic/build-android-release.sh
+++ Docker/ionic/build-android-release.sh
@@ -9,7 +9,7 @@ export NODE_OPTIONS="--max-old-space-size=8192"
 echo "Running build: build:stratis:android-release (release)"
 rm -rf /app/bitcore
 rm -rf /output/apk
-git clone https://github.com/stratisproject/bitcore-stratis.git --recurse-submodules --single-branch --branch release/2.0.0 /app/bitcore
+git clone https://github.com/stratisproject/bitcore-stratis.git --recurse-submodules --single-branch --branch latest /app/bitcore
 cd /app/bitcore
 npm i --unsafe-perm
 cd /app/bitcore/packages/mobile-wallet
@@ -21,5 +21,5 @@ cd /app/bitcore/packages/mobile-wallet
 npm run build:stratis:android-release
 cp -r /app/bitcore/packages/mobile-wallet/platforms/android/app/build/outputs/apk /output
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /keys/stratiswallet.keystore -storepass ${1} -signedjar /output/apk/release/app-release-signed.apk /output/apk/release/app-release-unsigned.apk  stratiswallet
-$ANDROID_HOME/build-tools/27.0.0/zipalign -v 4 /output/apk/release/app-release-signed.apk /output/apk/release/android-release-signed-aligned.apk
+$ANDROID_HOME/build-tools/30.0.3/zipalign -v 4 /output/apk/release/app-release-unsigned.apk --out /output/apk/release/app-release-signed-aligned.apk /output/apk/release/android-release-unsigned-aligned.apk
+$ANDROID_HOME/build-tools/30.0.3/apksigner sign --ks /keys/stratiswallet.keystore --ks-key-alias stratiswallet /output/apk/release/android-release-unsigned-aligned.apk
